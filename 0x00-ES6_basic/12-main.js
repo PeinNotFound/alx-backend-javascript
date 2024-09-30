73c02d1ba2bda6d4c1a7721f76dc1ab9c51eb99d
PeinNotFound <akatsuki_ahmed@yahoo.com>
2024-10-01 00:27:10 +0100

    making progress

---
+++ 0x00-ES6_basic/12-main.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/extensions
+import createEmployeesObject from './11-createEmployeesObject.js';
+// eslint-disable-next-line import/extensions
+import createReportObject from './12-createReportObject.js';
+
+const employees = {
+  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+  ...createEmployeesObject('marketing', ['Sylvie']),
+};
+
+// console.log(employees);
+const report = createReportObject(employees);
+console.log(report.allEmployees);
+console.log(report.getNumberOfDepartments(report.allEmployees));
